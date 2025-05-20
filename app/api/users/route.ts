32d39e6aa6afce76a80be100b216e2a51531f341
mano <manoa@livenexx.net>
2025-05-20 16:10:16 +0300

    [NEXT JS] : test API

---
+++ app/api/users/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  try {
+    const data = await prisma.user.findMany();
+
+    return NextResponse.json(
+      {
+        data,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: Request) => {
+  try {
+    const data = await req.json();
+
+    const res = await prisma.user.create({
+      data,
+    });
+
+    return NextResponse.json(res, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 }
+    );
+  }
+};
